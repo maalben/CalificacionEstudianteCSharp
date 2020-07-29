5f1f1000015f64109d5633d5acc5ef10ce25df28
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2020-07-28 20:13:09 -0500

    Calcular el promedio de las notas

---
+++ CalificacionEstudiante/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CalificacionEstudiante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.txtnota4 = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(190, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Promedio de Calificaciones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(39, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(292, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa primera nota del exámen:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(29, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingresa segunda nota del exámen:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(45, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(286, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingresa tercera nota del exámen:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(52, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(279, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ingresa cuarta nota del exámen:";
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota1.Location = new System.Drawing.Point(350, 133);
+            this.txtnota1.MaxLength = 4;
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(100, 38);
+            this.txtnota1.TabIndex = 5;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota2.Location = new System.Drawing.Point(350, 187);
+            this.txtnota2.MaxLength = 4;
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(100, 38);
+            this.txtnota2.TabIndex = 6;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota3.Location = new System.Drawing.Point(350, 239);
+            this.txtnota3.MaxLength = 4;
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(100, 38);
+            this.txtnota3.TabIndex = 7;
+            // 
+            // txtnota4
+            // 
+            this.txtnota4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota4.Location = new System.Drawing.Point(350, 290);
+            this.txtnota4.MaxLength = 4;
+            this.txtnota4.Name = "txtnota4";
+            this.txtnota4.Size = new System.Drawing.Size(100, 38);
+            this.txtnota4.TabIndex = 8;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(550, 247);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(181, 71);
+            this.btncalcular.TabIndex = 9;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 33);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Promedio:";
+            this.label6.Visible = false;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(216, 369);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(172, 41);
+            this.lblresultado.TabIndex = 11;
+            this.lblresultado.Text = "resultado";
+            this.lblresultado.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(593, 374);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtnota4);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.TextBox txtnota4;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CalificacionEstudiante/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalificacionEstudiante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+
+            double nota1, nota2, nota3, nota4, suma, resultado;
+            nota1 = double.Parse(txtnota1.Text);
+            nota2 = double.Parse(txtnota2.Text);
+            nota3 = double.Parse(txtnota3.Text);
+            nota4 = double.Parse(txtnota4.Text);
+
+            suma = nota1 + nota2 + nota3 + nota4;
+
+            resultado = suma / 4;
+
+            lblresultado.Text = Convert.ToString(resultado);
+
+            label6.Visible = true;
+            lblresultado.Visible = true;
+
+        }
+    }
+}
